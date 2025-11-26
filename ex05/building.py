0b4010c64ac02df1369c792743c3c36da62e228a
Fode Manca <fomanca@c2r5s11.42porto.com>
2025-11-26 19:47:44 +0000

    Implement building.py to analyze character types from input string

--- ex05/building.py
+++ ex05/building.py
@@ -1,29 +1,16 @@
-# **************************************************************************** #
-#                                                                              #
-#                                                         :::      ::::::::    #
-#    building.py                                        :+:      :+:    :+:    #
-#                                                     +:+ +:+         +:+      #
-#    By: fomanca <fomanca@student.42porto.com>      +#+  +:+       +#+         #
-#                                                 +#+#+#+#+#+   +#+            #
-#    Created: 2025/11/26 16:50:10 by fomanca           #+#    #+#              #
-#    Updated: 2025/11/26 17:25:50 by fomanca          ###   ########.fr        #
-#                                                                              #
-# **************************************************************************** #
-
 import sys
 from string import punctuation
+
 
 def main():
     if len(sys.argv) > 2:
         print("AssertionError: more than one argument is provided")
-        exit();
+        exit()
     elif len(sys.argv) == 2:
         mensagem_count = sys.argv[1]
     else:
         print("What is the text to count?")
         mensagem_count = sys.stdin.readline()
-
-
     spaces = [' ', '\n']
 
     print(f"The text contains {len(mensagem_count)} characters:")
@@ -32,7 +19,6 @@ def main():
     print(f"{sum(1 for c in mensagem_count if c in punctuation)} punctuation marks")
     print(f"{sum(1 for c in mensagem_count if c in spaces)} spaces")
     print(f"{sum(1 for c in mensagem_count if c.isdigit())} digits")
-
 
 
 if __name__ == "__main__":
